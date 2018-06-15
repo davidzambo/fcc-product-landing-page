45ff8f3976d773a15364834100885cf9bdd8ef37
David Zambo <david@dcmf.hu>
2018-06-15 05:10:32 +0200

    Css bugfix, watcher for active nav-link item

---
+++ js/scripts.js
@@ -0,0 +1,43 @@
+const links = document.getElementsByClassName('nav-link');
+let navLinks = [];
+let tmpSectionHeight = 0;
+
+for (link of links){
+    navLinks.push(link.getAttribute('data-target'));
+}
+
+const sectionPositions = navLinks.map(link => {
+    let l = document.querySelector(link);
+    tmpSectionHeight += parseInt(l.getBoundingClientRect().height);
+    return tmpSectionHeight;
+});
+console.log(sectionPositions);
+
+function toggleNavbar(){
+    const navbar = document.querySelector('#nav-bar');
+    navbar.classList.toggle('open');
+}
+
+for (link of links){
+    link.addEventListener('click', e => {
+        clearActiveTag();
+       e.target.parentNode.classList.add('active');
+    });
+}
+
+document.addEventListener('scroll', () => {
+   let scrollPosition = window.scrollY;
+   clearActiveTag();
+   for (let i = 0; i < sectionPositions.length; i++){
+       if (sectionPositions[i] - 300 > scrollPosition){
+           links[i].parentNode.classList.add('active');
+           break;
+       }
+   }
+});
+
+const clearActiveTag = () => {
+    for (el of links){
+        el.parentNode.classList.remove('active');
+    }
+}
